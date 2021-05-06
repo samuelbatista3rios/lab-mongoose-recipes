c733f7a5a602c1cdcfb3641319797ef4e85c1ad2
samuelbatista3rios <samuelbatista3rios@gmail.com>
2021-05-06 09:02:17 -0300

    done

--- index.js
+++ index.js
@@ -12,15 +12,42 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    // ITERATION 2
+    // const recipe = new Recipe({
+    //   title: 'Tacos',
+    //   level: 'Easy Peasy',
+    //   ingredients: ['Tortilla', 'Carne', 'Salsa'],
+    //   cuisine: 'Mexicana',
+    //   dishType: 'dessert',
+    //   duration: 30,
+    //   creator: 'No Se'
+    // })
+    // await recipe.save()
+
+    // ITERATION 3
+    // Recipe.insertMany(data)
+    //   .then(() => console.log("Data inserted"))
+    //   .catch(err => console.error(err))
+
+    // ITERATION 4
+    // Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, { $set: { duration: 100 } }, { new: true })
+    //   .then(recipe => console.log(recipe))
+    //   .catch(error => console.error(error))
+
+    // ITERATINO 5
+    Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(recipe => console.log(recipe))
+      .catch(error => console.error(error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
